dae5fce3f2984c1bfc4a45b9e7c4817107e6d0c8
designsbytraina <racheltrainagrandon@gmail.com>
2017-01-16 00:28:36 +0000

    Successfully got data from Accuweather API using requests library.

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-# from random import choice
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
+import os
 
+# Always keep your API Key secret
+API_KEY = os.environ['ACCUWEATHER_API_KEY']
 
 # "__name__" is a special Python variable for the name of the current module
 # Flask wants to know this to know what any imported things are relative to.
@@ -15,18 +18,52 @@ def index():
 
 
 @app.route('/wear-this')
-def show_result():
-    """Tell user what to wear."""
+
+@app.route('/get-weather', methods=['GET'])
+def show_results():
+    """Make request to Accuweather API and display result to user."""
 
     # Get form variable from request
     location = request.args.get('location')
 
+    payload = {'apikey':API_KEY, 'q':location, 'language':'en-us'}
+
+    response = requests.get('http://dataservice.accuweather.com/locations/v1/search', params=payload)
+
+    location_key = response.json()[0]['Key']
+
+    weather_data = get_data(location_key)
+
     return render_template('result.html',
-                            location=location)
+                            location=location,
+                            sentiment=weather_data['sentiment'],
+                            temp=weather_data['temp'],
+                            is_day=weather_data['is_day'])
 
-    return 'Location object appears here!'
+
+def get_data(location):
+    """Get location data and return to user."""
+
+    payload = {'apikey':API_KEY}
+
+    response = requests.get('http://dataservice.accuweather.com/currentconditions/v1/%s' % location, params=payload)
+
+    sentiment = response.json()[0]['WeatherText']
+
+    is_daytime = response.json()[0]['IsDayTime']
+
+    curr_temp = response.json()[0]['Temperature']['Imperial']['Value']
+
+    weather_obj = {'temp':curr_temp, 'is_day':is_daytime, 'sentiment':sentiment}
+    
+    return weather_obj
+
+    # is_jacket_needed()
+
+# def is_jacket_needed():
 
 
+##################################
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads" our
     # web app if we change the code.
