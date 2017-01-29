d9bb1590db7882844426a00a349f5b3ec113d7c3
designsbytraina <racheltrainagrandon@gmail.com>
2017-01-29 01:09:15 +0000

    Added logic for ront end changes based on weather conditions. Tested and working.

--- server.py
+++ server.py
@@ -13,6 +13,14 @@ API_KEY = os.environ['ACCUWEATHER_API_KEY']
 # Flask wants to know this to know what any imported things are relative to.
 app = Flask(__name__)
 
+# Global variables - color choices for background based on weather and time of day
+LT_BLUE = '#73C5FB'
+DK_BLUE = '#2E3261'
+LT_BLUE_GREY = '#81AFD6' 
+DK_BLUE_GREY = '#334A66'
+LT_GREY = '#A4A3C7'
+DK_GREY = '#41414F'
+
 
 ##################################
 # ROUTING
@@ -28,11 +36,11 @@ def index():
 
 
 @app.route('/get-weather', methods=['GET'])
-def show_results():
+def get_results():
     """Make request to Accuweather API and display result to user."""
 
     # Get form variable from request
-    location = request.args.get('location')
+    location = request.args.get('location').lower().capitalize()
 
     # Define payload for query to Accuweather API
     payload = {'apikey':API_KEY, 'q':location, 'language':'en-us'}
@@ -47,14 +55,96 @@ def show_results():
     weather_obj = get_current_weather(location_key)
 
     # TODO - Add logic for weather graphics and color changes
+    if weather_obj['temp'] <= 80.0:
+
+        if 'rain' in weather_obj['sentiment'].lower():
+            icon = 'rain'
+            is_jacket = True
+
+            if weather_obj['is_day']: # if True
+                bg_color = LT_GREY
+            else:
+                bg_color = DK_GREY
+
+        elif 'thunder' in weather_obj['sentiment'].lower():
+            icon = 'thunder'
+            is_jacket = True
+
+            if weather_obj['is_day']: # if True
+                bg_color = LT_GREY
+            else:
+                bg_color = DK_GREY
+
+        elif 'snow' in weather_obj['sentiment'].lower():
+            icon = 'snow'
+            is_jacket = True
+            bg_color = LT_GREY
+
+        elif 'sun' in weather_obj['sentiment'].lower():
+            icon = 'sun'
+
+            if weather_obj['is_day']: # if True
+                bg_color = LT_BLUE
+            else:
+                bg_color = DK_BLUE
+
+            if weather_obj['temp'] <= 65.0:
+                is_jacket = True
+            else:
+                is_jacket = False
+
+        elif 'cloud' in weather_obj['sentiment'].lower():
+            icon = 'cloud'
+
+            if weather_obj['is_day']: # if True
+                bg_color = LT_BLUE_GREY
+            else:
+                bg_color = DK_BLUE_GREY
+
+            if weather_obj['temp'] <= 65.0:
+                is_jacket = True
+            else:
+                is_jacket = False
+
+        else:
+            icon = 'cloud'
+            is_jacket = False
+
+            if weather_obj['is_day']:
+                bg_color = LT_BLUE
+            else:
+                bg_color = DK_BLUE
+
+    # Else if the temp is above 80F
+    else:
+        icon = 'cloud'
+        is_jacket = False
+
+        if weather_obj['is_day']: # if True
+            bg_color = LT_BLUE
+        else:
+            bg_color = DK_BLUE
+
     # TODO - Add logic for serving different messages based on weather conditions
+    if is_jacket:
+        message = 'You should wear a jacket'
+    else:
+        message = 'You should be okay without a jacket'
+
+    if weather_obj['is_day']:
+        font_color = '#000000'
+    else:
+        font_color = '#FFFFFF'
 
     # Render template passing weather object attributes to be accessible on the frontend
     return render_template('results.html',
+                            color=bg_color,
+                            font=font_color,
+                            icon=icon,
+                            message=message,
                             location=location,
                             sentiment=weather_obj['sentiment'],
-                            temp=weather_obj['temp'],
-                            is_day=weather_obj['is_day'])
+                            temp=weather_obj['temp'])
 
 
 # Process weather data using the location key passed as a parameter from show_results()
