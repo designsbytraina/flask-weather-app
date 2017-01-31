f1769ebd5df1053be0b35d4490124fef047509c1
Sang Tian <sangsta@dropbox.com>
2017-01-30 22:54:53 -0800

    Added tests

--- weather_ui.py
+++ weather_ui.py
@@ -10,6 +10,10 @@ DK_BLUE_GREY = '#334A66'
 LT_GREY = '#A4A3C7'
 DK_GREY = '#41414F'
 
+# Font colors
+LT_FONT = '#AEDFFF'
+DK_FONT = '#151A2B'
+
 def get_icon(description):
     """Get the icon based on the description of the weather."""
     if 'rain' in description:
@@ -44,13 +48,13 @@ def get_bg_color(icon, is_day):
 def get_font_color(is_day):
     """Get the font color based on time of day."""
     if is_day:
-        return '#151A2B'  # darker color
-    return '#AEDFFF'  # lighter color
+        return DK_FONT  # darker color
+    return LT_FONT  # lighter color
 
 def is_jacket(icon, temp):
     """Determine whether to bring a jacket based on the weather and the temperature."""
     is_bad_weather = icon in ('rain', 'thunder', 'snow')
-    is_cold = temp <= 80.0 or (icon in ('sun', 'cloud') and temp <= 65.0)
+    is_cold = temp <= 65.0
     return is_bad_weather or is_cold
 
 def get_ui_attributes(description, is_day, temp):
@@ -62,3 +66,28 @@ def get_ui_attributes(description, is_day, temp):
         'icon': icon,
         'is_jacket': is_jacket(icon, temp),
     }
+
+if __name__ == '__main__':
+    # Testing the code above!
+    # Note that this won't run when the module is imported,
+    # only when we decide to run it using python weather_ui.py
+
+    assert get_icon("It's raining today!") == 'rain'
+    assert get_icon("I love the sun!") == 'sun'
+    assert get_icon("I have no clue what the weather's like") == 'cloud'
+
+    assert get_bg_color('snow', True) == LT_GREY
+    assert get_bg_color('thunder', False) == DK_GREY
+    assert get_bg_color('cloud', True) == LT_BLUE_GREY
+    assert get_bg_color('cloud', False) == DK_BLUE_GREY
+    assert get_bg_color('sun', True) == LT_BLUE
+    assert get_bg_color('sun', False) == DK_BLUE
+
+    assert get_font_color(True) == DK_FONT
+    assert get_font_color(False) == LT_FONT
+
+    assert is_jacket('rain', 97.0)
+    assert is_jacket('sun', 60)
+    assert not is_jacket('cloud', 70.0)
+
+    print "All tests passed!"
